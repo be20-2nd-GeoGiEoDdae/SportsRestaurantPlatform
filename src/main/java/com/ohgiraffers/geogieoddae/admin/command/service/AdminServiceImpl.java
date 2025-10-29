34e4765f0eb513a01ca10cc81b575d048d0d8acf
silentanderson <anderson91@naver.com>
2025-10-29 16:49:43 +0900

    Feat: Admin login & logout 틀 잡아놓기

--- src/main/java/com/ohgiraffers/geogieoddae/admin/command/service/AdminServiceImpl.java
+++ src/main/java/com/ohgiraffers/geogieoddae/admin/command/service/AdminServiceImpl.java
@@ -1,6 +1,6 @@
 package com.ohgiraffers.geogieoddae.admin.command.service;
 
-public class AdminServiceImpl implements AdminService{
+public class AdminServiceImpl implements AdminService {
     @Override
     public void login() {
 
