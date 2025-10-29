f3596f3de24bea1a83e45ea7e7d33cb12a2d0993
silentanderson <anderson91@naver.com>
2025-10-29 16:43:02 +0900

    docs : 커밋메시지 템플릿 추가
    - .gitmessage.txt 템플릿을 작성하여 등록함

---
+++ src/main/java/com/ohgiraffers/geogieoddae/admin/command/service/AdminServiceImpl.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.geogieoddae.admin.command.service;
+
+public class AdminServiceImpl implements AdminService{
+    @Override
+    public void login() {
+
+    }
+
+    public void logout() {
+        
+    }
+}
