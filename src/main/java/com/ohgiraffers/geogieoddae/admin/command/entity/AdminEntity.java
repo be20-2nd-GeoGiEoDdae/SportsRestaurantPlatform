7038eb2f2247249e291a4163b93f4d121d52719e
seong-corder <tjdguswp333@gmail.com>
2025-10-31 16:44:08 +0900

    feat:공지사항 등록 기능 추가
    # Conflicts:
    #	src/main/java/com/ohgiraffers/geogieoddae/admin/command/entity/AdminEntity.java

--- src/main/java/com/ohgiraffers/geogieoddae/admin/command/entity/AdminEntity.java
+++ src/main/java/com/ohgiraffers/geogieoddae/admin/command/entity/AdminEntity.java
@@ -1,22 +1,9 @@
 package com.ohgiraffers.geogieoddae.admin.command.entity;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 import com.ohgiraffers.geogieoddae.announcement.command.entity.AnnouncementEntity;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
 
 @Entity
 @Table(name = "admin")
