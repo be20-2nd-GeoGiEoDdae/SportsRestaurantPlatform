458e7715e85d1f743002814f93d50b44c1920da9
KyujinPark01 <rbwls1100@naver.com>
2025-10-31 11:16:06 +0900

    refactor: change package structure

--- src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/repository/BookmarkRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ohgiraffers.geogieoddae.restaurant.command.repository;
-
-import com.ohgiraffers.geogieoddae.restaurant.command.entity.bookmark.BookmarkEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookmarkRepository extends JpaRepository<BookmarkEntity,Long> {
-}
--- src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/repository/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ohgiraffers.geogieoddae.restaurant.command.repository;
-
-import com.ohgiraffers.geogieoddae.restaurant.command.entity.review.ReviewEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
-}
