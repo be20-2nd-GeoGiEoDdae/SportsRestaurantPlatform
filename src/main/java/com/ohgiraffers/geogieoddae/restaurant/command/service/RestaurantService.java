92f4c1444daec3a282c38717c0fd48dc69d56aa0
KyujinPark01 <rbwls1100@naver.com>
2025-10-31 11:13:17 +0900

    feat: implement keyword and restaurant association feature

--- src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/service/RestaurantService.java
+++ src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/service/RestaurantService.java
@@ -1,4 +1,13 @@
 package com.ohgiraffers.geogieoddae.restaurant.command.service;
 
-public class RestaurantService {
+import com.ohgiraffers.geogieoddae.restaurant.command.dto.RestaurantDto;
+
+public interface RestaurantService {
+    //가게 등록
+    void registerRestaurant(RestaurantDto restaurantDto);
+    //가게 삭제
+    void deleteRestaurant(Long RestaurantId);
+    //가게 수정
+//    void updateRestaurant(Long RestaurantId, RestaurantDto restaurantDto);
+
 }
