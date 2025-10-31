bc345f5d63db7cf00e5b108d641c804ef46ca55e
KyujinPark01 <rbwls1100@naver.com>
2025-10-31 14:48:04 +0900

    feat : implement bookmark feature

--- src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/service/BookmarkService.java
+++ src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/service/BookmarkService.java
@@ -1,7 +1,10 @@
 package com.ohgiraffers.geogieoddae.restaurant.command.service;
 
 public interface BookmarkService {
-    //즐겨찾기 가게 조회
-    //즐겨찾기 가게 삭제
+
     //즐겨찾기 등록
+    void registerBookmark(Long userId, Long restaurantId);
+    //즐겨찾기 삭제
+    void deleteBookmark(Long id);
+
 }
