e15092ced510b1b19c015a52294212258cba8647
KyujinPark01 <rbwls1100@naver.com>
2025-10-31 14:32:27 +0900

    refact : entity update

--- src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/entity/restaurant/RestaurantEntity.java
+++ src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/entity/restaurant/RestaurantEntity.java
@@ -3,11 +3,15 @@ package com.ohgiraffers.geogieoddae.restaurant.command.entity.restaurant;
 import com.ohgiraffers.geogieoddae.auth.command.entity.entrepreneur.EntrepreneurEntity;
 import com.ohgiraffers.geogieoddae.global.common.entity.BaseTimeEntity;
 import com.ohgiraffers.geogieoddae.report.command.entity.blacklist.RestaurantBlacklistEntity;
-import com.ohgiraffers.geogieoddae.restaurant.entity.RestaurantKeywordEntity;
+import com.ohgiraffers.geogieoddae.restaurant.command.dto.KeywordDto;
+import com.ohgiraffers.geogieoddae.restaurant.command.dto.PictureDto;
+import com.ohgiraffers.geogieoddae.restaurant.command.dto.RestaurantDto;
+import com.ohgiraffers.geogieoddae.restaurant.command.entity.keyword.KeywordEntity;
+import com.ohgiraffers.geogieoddae.restaurant.command.entity.keyword.RestaurantKeywordEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -46,16 +50,28 @@ public class RestaurantEntity extends BaseTimeEntity {
     @Column(name = "restaurant_score")
     private Integer restaurantScore;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "entrepreneur_code", nullable = false)
-    private EntrepreneurEntity entrepreneur;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "entrepreneur_code", nullable = false)
+//    private EntrepreneurEntity entrepreneur;
 
-    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL)
-    private List<RestaurantPictureEntity> pictures;
+//    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<RestaurantPictureEntity> pictures;
 
-    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL)
-    private List<RestaurantKeywordEntity> keywords;
+//    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL,orphanRemoval = true)
+//    private List<RestaurantKeywordEntity> keywords;
 
-    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL,orphanRemoval = true)
     private List<RestaurantBlacklistEntity> blacklists;
-}
+
+//    public static void updatedRestaurant(RestaurantDto dto, RestaurantEntity restaurant) {
+//        restaurant.setRestaurantName(dto.getRestaurantName());
+//        restaurant.setRestaurantLocation(dto.getRestaurantLocation());
+//        restaurant.setRestaurantCategory(dto.getRestaurantCategory());
+//        restaurant.setRestaurantPeopleNumber(dto.getRestaurantPeopleNumber());
+//        restaurant.setRestaurantContents(dto.getRestaurantContents());
+//        restaurant.setRestaurantScore(dto.getRestaurantScore());
+//    }
+
+    }
+
+
