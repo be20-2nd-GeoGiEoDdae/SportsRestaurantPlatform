12e4d71d19d1719bac5ae5914e49d7396417a2b5
KyujinPark01 <rbwls1100@naver.com>
2025-10-31 14:47:17 +0900

    fix: structure update and conflict resolution

--- src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/repository/restaurant/RestaurantKeywordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ohgiraffers.geogieoddae.restaurant.command.repository.restaurant;
-
-import com.ohgiraffers.geogieoddae.restaurant.command.entity.keyword.RestaurantKeywordEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RestaurantKeywordRepository extends JpaRepository<RestaurantKeywordEntity,Long> {
-}
