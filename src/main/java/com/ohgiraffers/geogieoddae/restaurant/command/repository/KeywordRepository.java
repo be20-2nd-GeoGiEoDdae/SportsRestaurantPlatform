fe936d41fe7f4ba45c638ac11b634351c64c8a2c
KyujinPark01 <rbwls1100@naver.com>
2025-10-31 11:16:06 +0900

    refactor: change package structure

--- src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/repository/KeywordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ohgiraffers.geogieoddae.restaurant.command.repository;
-
-import com.ohgiraffers.geogieoddae.restaurant.command.entity.keyword.KeywordEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface KeywordRepository extends JpaRepository<KeywordEntity, Long> {
-}
