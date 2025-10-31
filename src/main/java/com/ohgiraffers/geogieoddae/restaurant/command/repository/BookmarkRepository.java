1b1d0f6e8c20098464534babf86e3437a12b30ad
KyujinPark01 <rbwls1100@naver.com>
2025-10-31 11:13:17 +0900

    feat: implement keyword and restaurant association feature
    # Conflicts:
    #	src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/controller/KeywordController.java
    #	src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/controller/RestaurantController.java
    #	src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/dto/KeywordDto.java
    #	src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/dto/PictureDto.java
    #	src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/dto/RestaurantDto.java
    #	src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/entity/keyword/RestaurantKeywordKey.java
    #	src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/repository/RestaurantRepository.java
    #	src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/service/BookmarkService.java
    #	src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/service/KeywordService.java
    #	src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/service/RestaurantService.java
    #	src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/service/ReviewService.java

---
+++ src/main/java/com/ohgiraffers/geogieoddae/restaurant/command/repository/BookmarkRepository.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.geogieoddae.restaurant.command.repository;
+
+import com.ohgiraffers.geogieoddae.restaurant.command.entity.bookmark.BookmarkEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookmarkRepository extends JpaRepository<BookmarkEntity,Long> {
+}
