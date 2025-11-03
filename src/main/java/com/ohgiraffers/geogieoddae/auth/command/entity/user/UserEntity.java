2ae9c2d54d32eb669e5f48efb28a38f87ff95e56
silentanderson <anderson91@naver.com>
2025-11-04 01:31:40 +0900

    Feat: 유저엔티티 소셜 토큰 컬럼 추가

--- src/main/java/com/ohgiraffers/geogieoddae/auth/command/entity/user/UserEntity.java
+++ src/main/java/com/ohgiraffers/geogieoddae/auth/command/entity/user/UserEntity.java
@@ -34,15 +34,28 @@ public class UserEntity extends BaseTimeEntity {
         private String userAddress;
 
 
-        @Column(name = "user_refresh_token", nullable = false)
+        @Column(name = "user_refresh_token", nullable = true)
         private String userRefreshToken;
 
-        @Column(name = "user_refresh_token_expires_at", nullable = false)
+        @Column(name = "user_refresh_token_expires_at", nullable = true)
         private LocalDateTime userRefreshTokenExpiresAt;
 
         @Enumerated(EnumType.STRING)
         @Column(name = "user_role", columnDefinition = "ENUM('USER','ENTREPRENEUR')")  // UserRole 에 맞게 영어로 수정 - 성현
         private UserRole userRole;
+
+        // 예시: UserEntity.java
+        @Column(nullable = true)
+        private String kakaoAccessToken;                    // 카카오 억세스토큰
+
+        @Column(nullable = true)
+        private String kakaoRefreshToken;                   // 카카오 리프레쉬토큰
+
+        @Column(nullable = true)
+        private LocalDateTime kakaoAccessTokenExpiresAt;    // 카카오 억세스토큰 만료
+
+        @Column(nullable = true)
+        private LocalDateTime kakaoRefreshTokenExpiresAt;   // 카카오 리프레쉬토큰 만료
 
         // ✅ 관계 매핑
         @OneToMany(mappedBy = "member")
