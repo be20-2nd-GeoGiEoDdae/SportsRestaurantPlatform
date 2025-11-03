ac9cea4a4c3bcf84710055f693d09b816c5cd848
silentanderson <anderson91@naver.com>
2025-11-04 01:04:01 +0900

    feat : 소셜 회원가입 기능 추가

--- src/main/java/com/ohgiraffers/geogieoddae/auth/command/controller/SocialSignupController.java
+++ src/main/java/com/ohgiraffers/geogieoddae/auth/command/controller/SocialSignupController.java
@@ -1,4 +1,27 @@
 package com.ohgiraffers.geogieoddae.auth.command.controller;
 
+import com.ohgiraffers.geogieoddae.auth.command.dto.SocialSignupRequest;
+import com.ohgiraffers.geogieoddae.auth.command.service.SocialSignupService;
+import com.ohgiraffers.geogieoddae.global.common.dto.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class SocialSignupController {
+
+    private final SocialSignupService socialSignupService;
+
+    public SocialSignupController(SocialSignupService socialSignupService) {
+        this.socialSignupService = socialSignupService;
+    }
+
+    @PostMapping("/social-signup")
+    public ResponseEntity<ApiResponse> socialSignup(@RequestBody SocialSignupRequest request) {
+        socialSignupService.socialSignup(request);
+        return ResponseEntity.ok(ApiResponse.success("회원가입 완료"));
+    }
 }
--- src/main/java/com/ohgiraffers/geogieoddae/auth/command/dto/SocialSignupRequest.java
+++ src/main/java/com/ohgiraffers/geogieoddae/auth/command/dto/SocialSignupRequest.java
@@ -1,4 +1,19 @@
 package com.ohgiraffers.geogieoddae.auth.command.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
 public class SocialSignupRequest {
+    private String userEmail;            // 소셜에서 받은 이메일
+    private String userName;             // 추가 입력
+    private String userPhoneNumber;      // 추가 입력
+    private String userAddress;          // 추가 입력
+    private String socialId;             // 소셜 ID
+    private String socialType;           // KAKAO, NAVER, GOOGLE 등
 }
