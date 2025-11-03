d4b6eb97d3db3143f65630feff9fcdffd99a509a
silentanderson <anderson91@naver.com>
2025-11-04 01:00:35 +0900

    feat: 회원엔티티 소셜 토큰 추가 및 Nullable 수정

---
+++ src/main/java/com/ohgiraffers/geogieoddae/auth/command/dto/SocialSignupRequest.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.geogieoddae.auth.command.dto;
+
+public class SocialSignupRequest {
+}
---
+++ src/main/java/com/ohgiraffers/geogieoddae/auth/command/service/SocialLogoutService.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.geogieoddae.auth.command.controller;
+
+import org.springframework.stereotype.Service;
+
+@Service
+class SocialLogoutService {
+
+}
