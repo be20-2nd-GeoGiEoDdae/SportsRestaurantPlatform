306c81223bafd4eff579e440776cd4041d322aaa
KyujinPark01 <rbwls1100@naver.com>
2025-11-04 16:51:01 +0900

    refator: 주석 수정

--- src/main/java/com/ohgiraffers/geogieoddae/viewing/query/controller/ViewingController.java
+++ src/main/java/com/ohgiraffers/geogieoddae/viewing/query/controller/ViewingController.java
@@ -1,4 +1,12 @@
 package com.ohgiraffers.geogieoddae.viewing.query.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping
 public class ViewingController {
 }
---
+++ src/main/java/com/ohgiraffers/geogieoddae/viewing/query/mapper/ViewingMapper.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.geogieoddae.viewing.query.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ViewingMapper {
+}
